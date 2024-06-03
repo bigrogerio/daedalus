1ac064d6187ada375f3062c04a5bcc9509b5ecbf
Gabriel Nicolaz Nagaoka <BRNGA049@iMac-de-Mastersupmbi.local>
2024-06-03 10:45:22 -0300

    adding docstrings and updating readme

--- daedalus/utils/dag_parse.py
+++ daedalus/utils/dag_parse.py
@@ -2,9 +2,17 @@ import ast
 import argparse
 import os
 import fnmatch
-import csv
 
 def extract_imports(file_path):
+    """
+    Extracts all import statements from a given Python file.
+
+    Parameters:
+    file_path (str): The path to the Python file from which to extract import statements.
+
+    Returns:
+    list: A list of unique import statements found in the file.
+    """
     with open(file_path, 'r') as file:
         tree = ast.parse(file.read(), filename=file_path)
 
@@ -47,6 +55,15 @@ def find_python_files(directory='.'):
     return python_files
 
 def extract_file_references(filename):
+    """
+    Extracts all file references from 'open' function calls in a given Python file.
+
+    Parameters:
+    filename (str): The path to the Python file from which to extract file references.
+
+    Returns:
+    list: A list of AST nodes representing the file references found in 'open' function calls.
+    """
     open_file_references= []
     with open(filename, 'r') as file:
         tree = ast.parse(file.read(), filename=filename)
@@ -57,6 +74,16 @@ def extract_file_references(filename):
     return open_file_references
 
 def resolve_ast_name_assigns(ast_tree, variable_definitions={}):
+    """
+    Resolves assignments in an AST tree, updating variable states.
+
+    Parameters:
+    ast_tree (list): The abstract syntax tree (AST) containing nodes to resolve.
+    variable_definitions (dict, optional): Dictionary of variable definitions to use in resolving Airflow variables.
+
+    Returns:
+    dict: A dictionary with variable names as keys and their resolved values.
+    """
     assign_node_list=[node for node in ast_tree if isinstance(node, ast.Assign)]
     resolve_dict={}
     state={}
@@ -76,6 +103,16 @@ def resolve_ast_name_assigns(ast_tree, variable_definitions={}):
     return state
 
 def resolve_joined_string(ast_JoinedStr_node, state):
+    """
+    Resolves a joined string node from an AST, replacing variables with their values.
+
+    Parameters:
+    ast_JoinedStr_node (ast.JoinedStr): The AST node representing the joined string.
+    state (dict): Dictionary containing the current state of variable values.
+
+    Returns:
+    str: The resolved string with variables replaced by their values.
+    """
     resolved=''
     if not isinstance(ast_JoinedStr_node, ast.JoinedStr):
         return resolved
@@ -89,17 +126,45 @@ def resolve_joined_string(ast_JoinedStr_node, state):
         return resolved
     
 def is_airflow_variable_call(ast_node):
+    """
+    Checks if a given AST node represents an Airflow Variable.get() call.
+
+    Parameters:
+    ast_node (ast.AST): The AST node to check.
+
+    Returns:
+    bool: True if the node represents an Airflow Variable.get() call, False otherwise.
+    """
     if isinstance(ast_node, ast.Call):
         if ast.dump(ast_node.func)=="Attribute(value=Name(id='Variable', ctx=Load()), attr='get', ctx=Load())":
             return True
     return False
 
 def get_airflow_variable_from_call(ast_node):
+    """
+    Extracts the variable name from an Airflow Variable.get() call AST node.
+
+    Parameters:
+    ast_node (ast.AST): The AST node representing the Airflow Variable.get() call.
+
+    Returns:
+    str or None: The name of the variable if found, otherwise None.
+    """
     if isinstance(ast_node, ast.Call):
         return ast_node.args[0].value
     return None
 
 def resolve_airflow_variable(ast_node, variable_definitions):
+    """
+    Resolves the value of an Airflow variable from an AST node using provided variable definitions.
+
+    Parameters:
+    ast_node (ast.AST): The AST node representing the Airflow Variable.get() call.
+    variable_definitions (dict): Dictionary containing variable definitions.
+
+    Returns:
+    str: The resolved variable value if found, otherwise 'undefined_variable_reference'.
+    """
     if isinstance(ast_node, ast.Constant):
         if ast_node.args[0].value in list(variable_definitions.keys()):
             return variable_definitions[ast_node.args[0].value]
